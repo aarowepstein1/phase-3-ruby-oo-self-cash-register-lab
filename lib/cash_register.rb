171587bfd129b9883dd341908a1d5e9cae6e7ee1
Aarow Epstein <aarowepstein1@gmail.com>
2023-06-19 16:45:21 -0700

    commit tree 38c7604f9fdc07f4ed9b44d02c794f4a87434a3c

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,49 @@
+class CashRegister
+    attr_reader :discount
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    def total
+        @total
+    end
+
+    def total=(total)
+        @total = total
+    end
+
+    def add_item(title, price , quanity = 1)
+        @title = title
+        @price = price
+        self.items=(title)
+        self.total= (price.to_f * quanity) + @total.to_f
+    end
+
+    def apply_discount
+        if @discount > 0
+            self.total = @total - (@total * @discount / 100)
+            "After the discount, the total comes to $#{@total.to_i}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def items=(title)
+        @items << title
+    end
+
+    def items(*args)
+        if args == 3
+            @items
+        else
+            @items.uniq
+    end
+
+    def void_last_transaction
+        @total - @price
+    end
+end
 
